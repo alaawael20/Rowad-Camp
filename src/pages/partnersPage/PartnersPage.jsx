327279eb8cfaf8ab45127fe8bc808b69eeaa8132
Alaa El-faqawi <93674732+alaaelfaqawi@users.noreply.github.com>
2025-08-11 20:56:16 +0300

    Add some partners

--- src/pages/partnersPage/PartnersPage.jsx
+++ src/pages/partnersPage/PartnersPage.jsx
@@ -4,6 +4,7 @@ import { partners } from "../../mock/partners";
 import Title from "../../components/title/Title";
 
 const PartnersPage = () => {
+  const defaultLogo = "/assets/partners/avatar.png";
   return (
     <section className="partners-section">
       <Title title="الشركاء والداعمون" />
@@ -12,9 +13,13 @@ const PartnersPage = () => {
         {partners.map((partner, index) => (
           <div key={index} className="partner-card">
             <img
-              src={partner.logo}
+              src={partner.logo || defaultLogo}
               alt={partner.name}
               className="partner-logo"
+              onError={(e) => {
+                e.target.onerror = null; // لتجنب الحلقة اللا نهائية
+                e.target.src = defaultLogo;
+              }}
             />
             <h3 className="partner-name">{partner.name}</h3>
             <p className="partner-description">{partner.description}</p>
