5e5555db0d4118ee0145e8d2d2cf2754ebbc6b1b
Alaa El-faqawi <93674732+alaaelfaqawi@users.noreply.github.com>
2025-08-25 08:16:40 +0300

    add new slider

--- src/mock/slides.js
+++ src/mock/slides.js
@@ -1,27 +1,34 @@
 export const slides = [
   {
     id: 1,
+    src: "/assets/slider/slider4.png",
+    alt: "System Slide",
+    title: "مخيم رواد بالشراكة مع جمعية الخريجات",
+    text: "تم تنصيب نظام محوسب لإدارة مراكز الإيواء في قطاع غزة",
+  },
+  {
+    id: 2,
     src: "/assets/slider/slider1.jpg",
     alt: "First Slide",
     title: "مخيم رواد",
     text: "مخيم رواد يقطن فيه النازحين في مواصي خانيونس",
   },
   {
-    id: 2,
+    id: 3,
     src: "/assets/slider/slider2.jpg",
     alt: "Second Slide",
     title: "مركز العمل التنموي معاً",
     text: "مشروع دعم التعليم والرفاهية وتنمية مهارات القيادة للأطفال في بيئات مؤقتة",
   },
   {
-    id: 3,
+    id: 4,
     src: "/assets/slider/slider3.jpg",
     alt: "Third Slide",
     title: "مبادرة رواد أجمل",
     text: "انطلاق مبادرة رواد أجمل تهدف على توعية النازحين على نظافة المخيم",
   },
   {
-    id: 4,
+    id: 5,
     src: "/assets/gallery/kids5.jpg",
     alt: "Forth Slide",
     title: "يوم ترفيهي خاص بالأطفال",
